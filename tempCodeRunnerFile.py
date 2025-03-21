ff58c9ebbb8a60dfeb84afd60dfc51e94a90e20f
Mantony2 <aman2004.jain@gmail.com>
2025-03-21 19:11:54 +0530

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-
-    print("Feature engineering completed.")
-    model, X_test, y_test, training_features = build_model(df)
-    print("Model training completed.")
-    evaluate_model(model, X_test, y_test)
-    
-    new_movie = {
-        'Title': 'Future Blockbuster',
-        'Director': 'John Doe',
